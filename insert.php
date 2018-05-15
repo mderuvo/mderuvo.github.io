cfb652c571103aee89751839f1bc34f3c72eb53f
deruvom <micol.deruvo@gmail.com>
2018-05-15 16:01:52 +0200

    new insert.php

--- insert.php
+++ insert.php
@@ -6,7 +6,7 @@ $city=$_POST['city'];
 $phone=$_POST['phone'];
 $email=$_POST['email'];
 
-$to = "youremail@gmail.com";
+$to = "micol.deruvo@gmail.com";
 $subject = "Future Tutorials Contact Form Test";
 $message = " Name: " . $name . "\r\n City: " . $city . "\r\n Phone: " . $phone . "\r\n Email: " . $email;
 
